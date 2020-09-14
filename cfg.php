8f38e2a231afb8da732cca4f74abe0c0725c203f
deep183 <55308998+deep183@users.noreply.github.com>
2020-09-14 22:37:23 +0530

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,25 @@
+<?php
+// -*- Copyleft @ °✍⎯⚚͜͡SʰⁱNᵒᵇⁱ - 2020 -*-
+// -*- Config CashPlay -*-
+// -*- If you want to donate, at DOGE Wallet:
+// -*- DDsu3tCfqXbhoj1j84Ydq3qkBYNZZESC3Q
+//________________________________________________
+
+// Input your data in here || Tulis data kamu di bawah
+// Register at website => https://www.cashplay.info/?ref=57327
+
+$userAgent = 'Xxxxxxxxxxx';
+
+// data_CashPlay
+$ssidCP = 'Xxxxxxxxxxxxx'; // PHPSESSID
+$AutoLogin = 'Xxxxxxxxxx';
+$AccExist = 'Xxxxxxxxxxxx';
+
+// data_Cp-Point
+$ssidCP_Point = 'Xxxxxxxxxxxxxx'; // PHPSESSID
+$address = 'Xxxxxxxxxxxx';
+
+// data_Cp-Video
+$ssidCP_Video = 'Xxxxxxxxx'; // PHPSESSID
+
+?>
